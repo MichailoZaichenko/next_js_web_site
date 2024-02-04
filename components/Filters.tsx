d210788508e30ea6ac36c7a319f0eab23a8538e8
MichailoZaichenko <michailo.zaichenko@gmail.com>
2024-02-05 00:27:54 +0200

    add search and filters

---
+++ components/Filters.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+const links = ['all', 'breakfast', 'brunch', 'lunch', 'dinner']
+
+const Filters = () => {
+  return (
+    <ul className="text-white-800 body-text no-scrollbar flex w-full max-w-full gap-2 overflow-auto py-12 sm:max-w-2xl">
+        {links.map((link) => (
+            <button
+                key={link}
+                onClick={() => {}}
+                className="whitespace-nowrap rounded-lg px-8 py-2.5 capitalize"
+            >
+                {link}
+            </button>
+        ))}
+    </ul>
+  )
+}
+
+export default Filters
